357a673ee3b9a3242c3dd608e311f4c25017bed5
ol1ves <103908970+ol1ves@users.noreply.github.com>
2026-05-29 20:44:47 -0700

    remove Vec2 addition operator

--- util.cpp
+++ util.cpp
@@ -1,9 +1,5 @@
 #include <util.h>
 
-Vec2 Vec2::operator+(const Vec2& rhs) const {
-    return { x + rhs.x, y + rhs.y };
-}
-
 Vec2& Vec2::operator+=(const Vec2& rhs) {
     x += rhs.x;
     y += rhs.y;
