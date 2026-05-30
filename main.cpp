040ca1482051b4b3987f78547bf40fc74c271f67
ol1ves <103908970+ol1ves@users.noreply.github.com>
2026-05-29 20:18:59 -0700

    basic apple gen + movement

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <ncurses.h>
+#include <random>
+#include <chrono>
+#include <thread>
+
+struct Vec2 {
+    int x;
+    int y;
+
+    Vec2 operator+(const Vec2& rhs) const {
+        return { x + rhs.x, y + rhs.y };
+    }
+
+    Vec2& operator+=(const Vec2& rhs) {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+};
+
+int main() {
+    // Initialize ncurses
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    clear();
+    nodelay(stdscr, TRUE);
+
+    int maxX = COLS - 1;
+    int maxY = LINES - 1;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist_x(1, maxX);
+    std::uniform_int_distribution<> dist_y(1, maxY);
+
+    Vec2 applePos = { dist_x(gen), dist_y(gen) };
+    Vec2 headPos = { maxX / 2, maxY / 2 };
+    Vec2 direction = { 1, 0 };
+
+    while (true) {
+        clear();
+        mvaddch(applePos.y, applePos.x, '@');
+        mvaddch(headPos.y, headPos.x, '*');
+        headPos += direction;
+        refresh();
+
+        
+        int ch = getch();
+        if (ch != ERR) {
+            switch (ch) {
+                case 'w': direction = { 0, -1 }; break;
+                case 's': direction = { 0, 1 }; break;
+                case 'a': direction = { -1, 0 }; break;
+                case 'd': direction = { 1, 0 }; break;
+            } 
+        }
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    endwin();
+    return 0;
+}
